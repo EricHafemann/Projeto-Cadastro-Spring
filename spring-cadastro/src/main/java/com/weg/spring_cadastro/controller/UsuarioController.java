9e6f2c4632c3dc0030acf1f0b0afce76a4bf1c48
Eric Hafemann <eric070gabriel@gmail.com>
2025-12-09 21:19:25 -0300

    🐛 Fix: Corrige erro na porta do Spring

--- spring-cadastro/src/main/java/com/weg/spring_cadastro/controller/UsuarioController.java
+++ spring-cadastro/src/main/java/com/weg/spring_cadastro/controller/UsuarioController.java
@@ -1,5 +1,14 @@
 package com.weg.spring_cadastro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
     
+    @GetMapping("/usuarios")
+    public String texto ()
+    {
+        return "Acessando a API";
+    }
 }
